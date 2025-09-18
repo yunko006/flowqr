154da736a779a747ca2fb2d3e0fa9ff6aead68e4
yunko006 <peyrachethomas@gmail.com>
2025-09-19 00:30:30 +0200

    Update auth client with polar

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,9 @@
 import { createAuthClient } from "better-auth/react";
+import { polarClient } from "@polar-sh/better-auth";
+import { organizationClient } from "better-auth/client/plugins";
+
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
   baseURL: "http://localhost:3000",
+  plugins: [polarClient()],
 });
