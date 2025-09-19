83c1392148f52195f19094e847dcc89350c817cc
yunko006 <peyrachethomas@gmail.com>
2025-09-20 00:12:53 +0200

    Create group route dashboard

---
+++ src/app/(dashboard)/qr-codes/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function HelloWorldPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">ID QR Codes</h1>
+    </div>
+  );
+}
---
+++ src/app/(dashboard)/qr-codes/page.tsx
@@ -0,0 +1,7 @@
+export default function HelloWorldPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Qr Codes</h1>
+    </div>
+  );
+}
