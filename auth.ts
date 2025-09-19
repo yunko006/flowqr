d91da3cb2ef10ea9e2ceb0545f69f391a8a7bd99
yunko006 <peyrachethomas@gmail.com>
2025-09-20 00:12:10 +0200

    Mode: sandbox

--- auth.ts
+++ auth.ts
@@ -13,6 +13,7 @@ import { Polar } from "@polar-sh/sdk";
 
 const polarClient = new Polar({
   accessToken: process.env.POLAR_ACCESS_TOKEN,
+  server: "sandbox", // Use this option if you're using the sandbox environment - else use 'production' or omit the parameter
 });
 
 export const auth = betterAuth({
