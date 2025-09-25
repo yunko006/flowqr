752b48b5c751876b0a9f26e1e85a6f3b1b9d067a
yunko006 <peyrachethomas@gmail.com>
2025-09-25 23:52:04 +0200

    Create route api pour facturer usage de create qr

---
+++ src/app/api/create-qr/route.ts
@@ -0,0 +1,25 @@
+import { Polar } from "@polar-sh/sdk";
+import { NextResponse } from "next/server";
+
+const polar = new Polar({
+  accessToken: process.env["POLAR_ACCESS_TOKEN"] ?? "",
+  server: "sandbox",
+});
+
+export const GET = async (req: Request, res: Response) => {
+  const results = await polar.events.ingest({
+    events: [
+      {
+        name: "qr_creation",
+        // Replace with your logic to get the customer id
+        externalCustomerId: "3kdIYnW0yWZpU5bWzkzemZWUW2jFUYNi",
+        metadata: {
+          route: "/api/create-qr",
+          method: "GET",
+        },
+      },
+    ],
+  });
+
+  return NextResponse.json(results);
+};
